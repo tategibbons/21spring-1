5481ca90d2ed715bbddb41e2ce3b51221dbe6fdd
saasstudent <student@seattlacademy.software>
2017-05-02 11:03:31 -0700

    add imu and tiltHeading to page

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var nodeimu = require('nodeimu');
+var IMU = new nodeimu.IMU();
+function getData() {
+    IMU.getValue(function(err, data) {
+        if (err) throw err;
+        sensors = data;
+	console.log(sensors);
+    });
+}
+getData();
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+function getData() {
+    IMU.getValue(function(err, data) {
+        if (err) throw err;
+        sensors = data;
+	console.log(sensors);
+	io.emit("sensors",sensors)
+    });
+}
+
+setInterval(getData,1000);
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
